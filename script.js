5831ba7be361657ca76bb27721bcee4fe718cbd0
lukaszrychlik145-spec <lukaszrychlik145@gmail.com>
2026-02-06 10:37:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+(async function checkForUpdates() {
+    const currentVersion = "1.0";
+    const versionUrl = "https://raw.githubusercontent.com/ivysone/Will-you-be-my-Valentine-/main/version.json"; 
+
+    try {
+        const response = await fetch(versionUrl);
+        if (!response.ok) {
+            console.warn("Could not fetch version information.");
+            return;
+        }
+        const data = await response.json();
+        const latestVersion = data.version;
+        const updateMessage = data.updateMessage;
+
+        if (currentVersion !== latestVersion) {
+            alert(updateMessage);
+        } else {
+            console.log("You are using the latest version.");
+        }
+    } catch (error) {
+        console.error("Error checking for updates:", error);
+    }
+})();
+
+const messages = [
+    "Jesteś pewien?",
+    "Naprawdę??",
+    "Myślisz trzeźwo?",
+    "Małpko proszee...",
+    "Przemyśl to!",
+    "Jeśli nie to będzie mi smutno...",
+    "Będzie bardzo smutno...",
+    "Bardzo bardzo smutno...",
+    "Dobra juz nie pytam..",
+    "Żartowałem klikaj TAKKKK ! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
